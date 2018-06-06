54f3fc0bf4eab47f179bd197471fb2d0bb47b8b6
GMR516 <garrett@heywordboy.com>
2018-06-05 20:37:09 -0700

    Some cleanup.

--- SteamLauncher/Program.cs
+++ SteamLauncher/Program.cs
@@ -1,33 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp1
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string directory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        try
         {
-            string directory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-            try
+            string readAppID = File.ReadAllText(directory + $@"\{Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetExecutingAssembly().Location)}.txt");
+            int.TryParse(readAppID.Trim(), out int appID);
+            ProcessStartInfo steamApp = new ProcessStartInfo
             {
-                string readAppID = File.ReadAllText(directory + $@"\{Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetExecutingAssembly().Location)}.txt");
-                int.TryParse(readAppID, out int appID);
-                ProcessStartInfo steamApp = new ProcessStartInfo
-                {
-                    FileName = @"C:\Program Files (x86)\Steam\Steam.exe",
-                    Arguments = "-applaunch " + appID
-                };
-                Process.Start(steamApp);
-            }
-            catch (Exception e)
-            {
-                //Todo: Show some sort of window pop up or something. 
-            }
+                FileName = @"C:\Program Files (x86)\Steam\Steam.exe",
+                Arguments = "-applaunch " + appID
+            };
+            Process.Start(steamApp);
+        }
+        catch (Exception e)
+        {
+            //Todo: Show some sort of window pop up or something. 
         }
     }
 }
